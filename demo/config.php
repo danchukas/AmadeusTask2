c8a005626c711721d2f26b77d37a60336aca3100
Анатолий Данчук <anatoliy.danchuk.softimus@gmail.com>
2018-04-01 22:34:48 +0300

    add metrics, readme

--- demo/config.php
+++ demo/config.php
@@ -2,23 +2,23 @@
 declare(strict_types=1);
 
 // tasks from Amadeus
-return include __DIR__ . '/../src/ConfigLib/config__md5.php';
-//return include __DIR__ . '/../src/ConfigLib/config__md5_md5.php';
-//return include __DIR__ . '/../src/ConfigLib/config__sha1.php';
-//return include __DIR__ . '/../src/ConfigLib/config__md5_sha1.php';
-//return include __DIR__ . '/../src/ConfigLib/config__aes-256-cbc.php';
+return include __DIR__ . '/ConfigLib/config__md5.php';
+//return include __DIR__ . '/ConfigLib/config__md5_md5.php';
+//return include __DIR__ . '/ConfigLib/config__sha1.php';
+//return include __DIR__ . '/ConfigLib/config__md5_sha1.php';
+//return include __DIR__ . '/ConfigLib/config__aes-256-cbc.php';
 
-//return include __DIR__ . '/../src/ConfigLib/config__log_nothing.php';
-//return include __DIR__ . '/../src/ConfigLib/config__log_in.php';
-//return include __DIR__ . '/../src/ConfigLib/config__log_out.php';
-//return include __DIR__ . '/../src/ConfigLib/config__log_in_out.php';
+//return include __DIR__ . '/ConfigLib/config__log_nothing.php';
+//return include __DIR__ . '/ConfigLib/config__log_in.php';
+//return include __DIR__ . '/ConfigLib/config__log_out.php';
+//return include __DIR__ . '/ConfigLib/config__log_in_out.php';
 
 // for test decode possibility
-//return include __DIR__ . '/../src/ConfigLib/config__aes-256-cbc.php_md5.php';
-//return include __DIR__ . '/../src/ConfigLib/config__md5_aes-256-cbc.php';
+//return include __DIR__ . '/ConfigLib/config__aes-256-cbc.php_md5.php';
+//return include __DIR__ . '/ConfigLib/config__md5_aes-256-cbc.php';
 
 // any combination for hash
-//return include __DIR__ . '/../src/ConfigLib/config__md5_aes-256-cbc_sha1.php';
+//return include __DIR__ . '/ConfigLib/config__md5_aes-256-cbc_sha1.php';
 
 // any combination for encode
-//return include __DIR__ . '/../src/ConfigLib/config__aes-256-cbc_aes-256-cbc_aes-256-cbc.php';
+//return include __DIR__ . '/ConfigLib/config__aes-256-cbc_aes-256-cbc_aes-256-cbc.php';
